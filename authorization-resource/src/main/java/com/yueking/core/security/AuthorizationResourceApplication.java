24dd2eb878e6957c9a6c2fbb549f1f444d713401
yuewu <yuekinger@gmail.com>
2021-12-18 10:53:05 +0800

    init

---
+++ authorization-resource/src/main/java/com/yueking/core/security/AuthorizationResourceApplication.java
@@ -0,0 +1,13 @@
+package com.yueking.core.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationResourceApplication.class, args);
+    }
+
+}
