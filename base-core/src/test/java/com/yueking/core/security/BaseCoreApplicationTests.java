5c01e3556fd19f358199b457195014e7a49209da
yuewu <yuekinger@gmail.com>
2021-12-18 11:03:30 +0800

    base-core

--- base-core/src/test/java/com/yueking/core/security/BaseCoreApplicationTests.java
+++ base-core/src/test/java/com/yueking/core/security/BaseCoreApplicationTests.java
@@ -2,12 +2,20 @@ package com.yueking.core.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class BaseCoreApplicationTests {
+    @Resource
+    UserDetailsService userDetailsService;
 
     @Test
     void contextLoads() {
+        UserDetails userDetails = userDetailsService.loadUserByUsername("admin");
+        System.out.println(userDetails);
     }
 
 }
