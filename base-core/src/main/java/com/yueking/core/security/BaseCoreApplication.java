626be7df2102a164cea52a1e15e628344f479b1b
yuewu <yuekinger@gmail.com>
2021-12-18 10:48:41 +0800

    init

---
+++ base-core/src/main/java/com/yueking/core/security/BaseCoreApplication.java
@@ -0,0 +1,13 @@
+package com.yueking.core.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseCoreApplication.class, args);
+    }
+
+}
