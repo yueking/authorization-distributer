b3b7ae9c968f84c4e0adb27b33b6bacf37d55a34
yuewu <yuekinger@gmail.com>
2021-12-22 15:50:29 +0800

    token配置

--- authorization-server/src/main/java/com/yueking/core/security/config/JwtTokenStoreConfig.java
+++ authorization-server/src/main/java/com/yueking/core/security/config/JwtTokenStoreConfig.java
@@ -1,5 +1,6 @@
 package com.yueking.core.security.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
