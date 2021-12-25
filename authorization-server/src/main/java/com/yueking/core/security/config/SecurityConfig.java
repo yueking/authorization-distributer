509c6de0848a01b87054df3ba854dd3f55bd967a
yuewu <yuekinger@gmail.com>
2021-12-25 13:24:22 +0800

    cors问题授权服务

--- authorization-server/src/main/java/com/yueking/core/security/config/SecurityConfig.java
+++ authorization-server/src/main/java/com/yueking/core/security/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //测试页面不需要授权
-        http.authorizeRequests().antMatchers("/test").permitAll().and().httpBasic().and().csrf().disable();
+        http.authorizeRequests().antMatchers("/oauth/test").permitAll().and().httpBasic().and().csrf().disable();
         //所有请求都需要授权才能访问
         http.authorizeRequests().anyRequest().authenticated()
                 .and().httpBasic().and().csrf().disable();
--- authorization-server/src/main/java/com/yueking/core/security/controller/TestController.java
+++ authorization-server/src/main/java/com/yueking/core/security/controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @RequestMapping("/test")
+    @RequestMapping("/oauth/test")
     public Object test(){
         return "Authorization Server is ready";
     }
